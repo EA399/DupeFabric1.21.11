77be65f85c12de6994df8f3e9314b8ff61291cee
EA399 <efecan_a@icloud.com>
2026-08-14 17:28:42 +0200

    Update DupeFabricClient.java

--- src/main/java/ea399/dupefabric/DupeFabricClient.java
+++ src/main/java/ea399/dupefabric/DupeFabricClient.java
@@ -3,7 +3,6 @@ package ea399.dupefabric;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.text.Text;
@@ -34,30 +33,9 @@ public class DupeFabricClient implements ClientModInitializer {
                 }
 
                 client.player.sendMessage(
-                        Text.literal("§aDupeFabric: G erkannt – sende Anfrage..."),
+                        Text.literal("§aDupeFabric: G erkannt!"),
                         false
                 );
-
-                if (ClientPlayNetworking.canSend(
-                        DupeFabric.DupePayload.ID
-                )) {
-
-                    ClientPlayNetworking.send(
-                            new DupeFabric.DupePayload()
-                    );
-
-                    client.player.sendMessage(
-                            Text.literal("§aDupeFabric: Anfrage gesendet!"),
-                            false
-                    );
-
-                } else {
-
-                    client.player.sendMessage(
-                            Text.literal("§cDupeFabric: Server unterstützt die Mod nicht!"),
-                            false
-                    );
-                }
             }
         });
     }
